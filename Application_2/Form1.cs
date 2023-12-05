03dee0c03ae1ec0c505753d29866c8d61f6e2476
Aryanlathiya <104750315+Aryanlathiya@users.noreply.github.com>
2023-12-05 11:35:32 +0000

    made 2 class shapeRectangle and shapeRectangleFactroy. I will implement this 2 class.

--- Application_2/Form1.cs
+++ Application_2/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +13,18 @@ namespace Application_2
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+       
+        }
+
+       
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
