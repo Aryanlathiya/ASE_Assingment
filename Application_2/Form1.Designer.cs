3ff1e36cad60bf5631b4b11c9926ab1da3cd73f1
Aryanlathiya <104750315+Aryanlathiya@users.noreply.github.com>
2023-12-12 14:45:15 +0000

    Completed Command Parser class

--- Application_2/Form1.Designer.cs
+++ Application_2/Form1.Designer.cs
@@ -120,6 +120,7 @@
             this.button4.TabIndex = 21;
             this.button4.Text = "Clear";
             this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button3
             // 
@@ -131,6 +132,7 @@
             this.button3.TabIndex = 20;
             this.button3.Text = "Run";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
@@ -142,6 +144,7 @@
             this.button2.TabIndex = 19;
             this.button2.Text = "Save";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label1
             // 
