37fca89de3bc9f900b2b806bec00697754a67be9
Aryanlathiya <104750315+Aryanlathiya@users.noreply.github.com>
2024-03-28 20:26:54 +0000

    Implemented shapeFactory class

--- WpfApp_Reassesent/shapeFactory.cs
+++ WpfApp_Reassesent/shapeFactory.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace WpfApp_Reassesent
 {
+    /// <summary>
+    /// Represents an abstract factory for creating instances of shapes.
+    /// </summary>
     abstract class shapeFactory
     {
+
+
+        /// <summary>
+        /// Gets a shape instance created by the factory.
+        /// </summary>
+        /// <returns>A shape instance.</returns>
+
+        public abstract Shape GetShape();
     }
 }
